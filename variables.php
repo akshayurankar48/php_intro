68b79e0cadf8c917a61d37b141b4508a1f7c5fac
evilieswithin <evilieswithin>
2023-12-12 16:40:29 +0530

    intro to variables

---
+++ variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+
+<body>
+    <?php
+    $var1 = 10;
+    echo $var1;
+    $my_variable = "Hello World";
+    $my_Variable = "Hello There";
+    echo $my_Variable;
+    echo "<br/>";
+    $var1 = 100;
+    echo $var1;
+    ?>
+</body>
+
+</html>
