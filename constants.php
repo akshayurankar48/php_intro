2ffcd0fc0acd89f2f6c37d5228741ca7d6025f4b
evilieswithin <evilieswithin>
2023-12-13 10:26:54 +0530

    explore constants in php

---
+++ constants.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constants</title>
+</head>
+
+<body>
+    <?php
+    $max_width = 980;
+    define("MAX_WIDTH", 980);
+    echo MAX_WIDTH;
+    echo "<br/>";
+    $max_width += 1;
+    echo $max_width;
+    ?>
+</body>
+
+</html>
